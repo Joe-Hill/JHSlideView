de2ab924336594ddac12ed0a1ed2b1f30ae0684f
Joe Hill <one@joehill.me>
2016-01-29 23:03:29 +0800

    change UIWebView to WKWebView

---
+++ ProductReview/Supporting Files/JHColor.h
@@ -0,0 +1,13 @@
+//
+//  JHColor.h
+//  ProductReview
+//
+//  Created by Joe on 1/21/16.
+//  Copyright © 2016 Joe. All rights reserved.
+//
+
+#ifndef JHColor_h
+#define JHColor_h
+
+
+#endif /* JHColor_h */
---
+++ ProductReview/Supporting Files/JHLog.h
@@ -0,0 +1,13 @@
+//
+//  JHLog.h
+//  ProductReview
+//
+//  Created by Joe on 1/21/16.
+//  Copyright © 2016 Joe. All rights reserved.
+//
+
+#ifndef JHLog_h
+#define JHLog_h
+
+
+#endif /* JHLog_h */
